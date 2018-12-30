418beb36e03df40f3960328439ed0bd65f8259a1
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2018-12-30 16:22:41 -0700

    Initial Commit

---
+++ screensaver_00/screensaver_00View.h
@@ -0,0 +1,13 @@
+//
+//  screensaver_00View.h
+//  screensaver_00
+//
+//  Created by Mac User on 12/30/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface screensaver_00View : ScreenSaverView
+
+@end
---
+++ screensaver_00/screensaver_00View.m
@@ -0,0 +1,52 @@
+//
+//  screensaver_00View.m
+//  screensaver_00
+//
+//  Created by Mac User on 12/30/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+#import "screensaver_00View.h"
+
+@implementation screensaver_00View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
